887633f1319aecc302e0999ca3847feb47700d60
Derrick Ding <yingke.dev@gmail.com>
2022-05-12 14:00:20 +0800

    add rating attribute

--- MusicFit/MusicFit/Models/Song.swift
+++ MusicFit/MusicFit/Models/Song.swift
@@ -13,6 +13,13 @@ struct Song {
 	var artistName: String
 	var artworkURL: String  // Cover image URL
     var genreNames: [String]  // ["Pop", "Music", ...]
+	var likes: Rating
+	
+	enum Rating {
+		case likes
+		case dislikes
+		case unset
+	}
 	
     init(id: String, name: String, artistName: String, artworkURL: String, genreNames: [String]) {
 		self.id = id
@@ -20,5 +27,15 @@ struct Song {
 		self.artistName = artistName
 		self.artworkURL = artworkURL
         self.genreNames = genreNames
+		self.likes = .unset  // TODO: Maybe delete this initializer
+	}
+	
+	init(id: String, name: String, artistName: String, artworkURL: String, genreNames: [String], likes: Rating) {
+		self.id = id
+		self.name = name
+		self.artistName = artistName
+		self.artworkURL = artworkURL
+		self.genreNames = genreNames
+		self.likes = likes
 	}
 }
