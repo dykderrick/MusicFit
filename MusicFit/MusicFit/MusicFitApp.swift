510a0cf11abef073748da52d135daf9408fc2876
Derrick Ding <yingke.dev@gmail.com>
2023-06-25 01:01:55 -0700

    add constructor to the ContentView

--- MusicFit/MusicFit/MusicFitApp.swift
+++ MusicFit/MusicFit/MusicFitApp.swift
@@ -9,35 +9,35 @@ import SwiftUI
 
 @main
 struct MusicFitApp: App {
-	let musicManager: AppleMusicManager
-	let fileHandler: FileHandler
-	let contentViewIntentHandler: ContentViewIntentHandler
-	let miniPlayerIntentHandler: MiniPlayerIntentHandler
-	let musicFitPlaylistManager: MusicFitPlaylistManager
-	let workoutManager: WorkoutManager
-	let musicPlayer: MusicPlayer
-	
-	init() {
-		musicManager = AppleMusicManager()
-		fileHandler = FileHandler()
-		musicFitPlaylistManager = MusicFitPlaylistManager(musicManager: musicManager, fileHandler: fileHandler)
-		contentViewIntentHandler = ContentViewIntentHandler(musicFitPlaylistManager: musicFitPlaylistManager)
-		miniPlayerIntentHandler = MiniPlayerIntentHandler()
-		musicPlayer = MusicPlayer(fileHandler: fileHandler, musicManager: musicManager)
-		workoutManager = WorkoutManager(musicPlayer: musicPlayer)
-	}
-	
+    let musicManager: AppleMusicManager
+    let fileHandler: FileHandler
+    let contentViewIntentHandler: ContentViewIntentHandler
+    let miniPlayerIntentHandler: MiniPlayerIntentHandler
+    let musicFitPlaylistManager: MusicFitPlaylistManager
+    let workoutManager: WorkoutManager
+    let musicPlayer: MusicPlayer
+    
+    init() {
+        musicManager = AppleMusicManager()
+        fileHandler = FileHandler()
+        musicFitPlaylistManager = MusicFitPlaylistManager(musicManager: musicManager, fileHandler: fileHandler)
+        contentViewIntentHandler = ContentViewIntentHandler(musicFitPlaylistManager: musicFitPlaylistManager)
+        miniPlayerIntentHandler = MiniPlayerIntentHandler()
+        musicPlayer = MusicPlayer(fileHandler: fileHandler, musicManager: musicManager)
+        workoutManager = WorkoutManager(musicPlayer: musicPlayer)
+    }
+    
     var body: some Scene {
         WindowGroup {
-			ContentView(
-				musicManager: musicManager,
-				workoutManager: workoutManager,
-				musicPlayer: musicPlayer,
-				miniPlayerIntentHandler: miniPlayerIntentHandler,
-				fileHandler: fileHandler,
-				intentHandler: contentViewIntentHandler,
-				musicFitPlaylistManager: musicFitPlaylistManager
-			)
+            ContentView(
+                musicManager,
+                workoutManager,
+                musicPlayer,
+                miniPlayerIntentHandler,
+                fileHandler,
+                contentViewIntentHandler,
+                musicFitPlaylistManager
+            )
         }
     }
 }
--- MusicFit/MusicFit/Views/ContentView.swift
+++ MusicFit/MusicFit/Views/ContentView.swift
@@ -10,167 +10,188 @@ import StoreKit
 import SwiftUI
 
 struct ContentView: View {
-	// MARK: - ObservedObject variables
-	@ObservedObject var musicManager: AppleMusicManager
-	@ObservedObject var workoutManager: WorkoutManager
-	@ObservedObject var musicPlayer: MusicPlayer
-	@ObservedObject var miniPlayerIntentHandler: MiniPlayerIntentHandler
-	
-	let fileHandler: FileHandler
-	let intentHandler: ContentViewIntentHandler
-	let musicFitPlaylistManager: MusicFitPlaylistManager
-	@State private var selection = 0
-	
+    // MARK: - ObservedObject variables
+    @ObservedObject var musicManager: AppleMusicManager
+    @ObservedObject var workoutManager: WorkoutManager
+    @ObservedObject var musicPlayer: MusicPlayer
+    @ObservedObject var miniPlayerIntentHandler: MiniPlayerIntentHandler
+    
+    let fileHandler: FileHandler
+    let intentHandler: ContentViewIntentHandler
+    let musicFitPlaylistManager: MusicFitPlaylistManager
+    @State private var selection = 0
+    
+    init(
+        _ musicManager: AppleMusicManager,
+        _ workoutManager: WorkoutManager,
+        _ musicPlayer: MusicPlayer,
+        _ miniPlayerIntentHandler: MiniPlayerIntentHandler,
+        _ fileHandler: FileHandler,
+        _ intentHandler: ContentViewIntentHandler,
+        _ musicFitPlaylistManager: MusicFitPlaylistManager
+    ) {
+        self.musicManager = musicManager
+        self.workoutManager = workoutManager
+        self.musicPlayer = musicPlayer
+        self.miniPlayerIntentHandler = miniPlayerIntentHandler
+        self.fileHandler = fileHandler
+        self.intentHandler = intentHandler
+        self.musicFitPlaylistManager = musicFitPlaylistManager
+        self.selection = selection
+    }
+    
     var body: some View {
-		TabView(selection: $selection) {
-			NowView(
-				workoutManager: workoutManager,
-				musicPlayer: musicPlayer,
-				miniPlayerIntentHandler: miniPlayerIntentHandler
-			)
-				.tag(0)
-				.tabItem {
-					VStack {
-						Image(systemName: "music.note")  // TODO: Change image
-						Text("Now")
-					}
-				}
-			
-			PlaylistView(
-				musicManager: musicManager,
-				musicPlayer: musicPlayer,
-				miniPlayerIntentHandler: miniPlayerIntentHandler,
-				musicFitPlaylistManager: musicFitPlaylistManager
-			)
-				.tag(1)
-				.tabItem {
-					VStack {
-						Image(systemName: "text.append")
-						Text("Playlist")
-					}
-				}
-			
-			SearchView()
-				.tag(2)
-				.tabItem {
-					VStack {
-						Image(systemName: "magnifyingglass")
-						Text("Search")
-					}
-				}
-			
-			MeView()
-				.tag(3)
-				.tabItem {
-					VStack {
-						Image(systemName: "person.fill")
-						Text("Me")
-					}
-				}
-		}
-		.accentColor(Color(hex: "\(MusicFitColors.green)"))
-		.onAppear() {
-			// Copy Bundle file MusicFitPlaylists.json to App Documents directory.
-			intentHandler.setMusicFitPlaylistMetadata(fileHandler)
-			
-			// Make sure all playlists previously set are available. If not available, set to "".
-			intentHandler.setMusicFitPlaylistsAvailability(musicManager)
-			
-			SKCloudServiceController.requestAuthorization { (status) in
-				if status == .authorized {
-					musicManager.getUserToken { userToken in
-						print(userToken)
-						
-						/*
-						musicManager.createPlaylistWithCatelogSongs(userToken, playlistName: "test playlist", playlistDescription: "test description", playlistFolderId: "p.playlistsroot", songCatelogIds: ["1450695739", "1440811598"]) { playlist in
-							print(playlist)
-						}
-						 */
-						
-						/*
-						musicManager.getSongRating(userToken, id: "1544494722") { rating in
-							print(rating)
-						}
-						 */
-						
-						/*
-						musicManager.createLibraryPlaylistFolder(userToken, folderName: "test folder") { isCreated, folderId in
-							print(isCreated)
-							print(folderId)
-						}
-						 */
-						
-						print("RESTING TEST: \(musicFitPlaylistManager.getMusicFitPlaylistId(ofStatus: .Resting))")
-						
+        TabView(selection: $selection) {
+            NowView(
+                workoutManager: workoutManager,
+                musicPlayer: musicPlayer,
+                miniPlayerIntentHandler: miniPlayerIntentHandler
+            )
+            .tag(0)
+            .tabItem {
+                VStack {
+                    Image(systemName: "music.note")
+                    Text("Now")
+                }
+            }
+            
+            PlaylistView(
+                musicManager: musicManager,
+                musicPlayer: musicPlayer,
+                miniPlayerIntentHandler: miniPlayerIntentHandler,
+                musicFitPlaylistManager: musicFitPlaylistManager
+            )
+            .tag(1)
+            .tabItem {
+                VStack {
+                    Image(systemName: "text.append")
+                    Text("Playlist")
+                }
+            }
+            
+            SearchView()
+                .tag(2)
+                .tabItem {
+                    VStack {
+                        Image(systemName: "magnifyingglass")
+                        Text("Search")
+                    }
+                }
+            
+            MeView()
+                .tag(3)
+                .tabItem {
+                    VStack {
+                        Image(systemName: "person.fill")
+                        Text("Me")
+                    }
+                }
+        }
+        .accentColor(Color(hex: "\(MusicFitColors.green)"))
+        .onAppear() {
+            // Copy Bundle file MusicFitPlaylists.json to App Documents directory.
+            intentHandler.setMusicFitPlaylistMetadata(fileHandler)
+            
+            // Make sure all playlists previously set are available. If not available, set to "".
+            intentHandler.setMusicFitPlaylistsAvailability(musicManager)
+            
+            SKCloudServiceController.requestAuthorization { (status) in
+                if status == .authorized {
+                    musicManager.getUserToken { userToken in
+                        print(userToken)
+                        
+                        /*
+                         musicManager.createPlaylistWithCatelogSongs(userToken, playlistName: "test playlist", playlistDescription: "test description", playlistFolderId: "p.playlistsroot", songCatelogIds: ["1450695739", "1440811598"]) { playlist in
+                         print(playlist)
+                         }
+                         */
+                        
+                        /*
+                         musicManager.getSongRating(userToken, id: "1544494722") { rating in
+                         print(rating)
+                         }
+                         */
+                        
+                        /*
+                         musicManager.createLibraryPlaylistFolder(userToken, folderName: "test folder") { isCreated, folderId in
+                         print(isCreated)
+                         print(folderId)
+                         }
+                         */
+                        
+                        print("RESTING TEST: \(musicFitPlaylistManager.getMusicFitPlaylistId(ofStatus: .Resting))")
+                        
                         
                         musicManager.getAllLibraryPlaylists(userToken) { playlists in
                             print(playlists)
                         }
-
-						/*
-						musicManager.fetchStorefrontID(userToken: userToken) { storefrontID in
-							print(storefrontID)
-							musicManager.searchAppleMusic(userToken, storefrontID, "Taylor Swift") { songs in
-								print(songs)
-							}
-						}
-						 */
-					}
-				}
-			}
-		}
-		// When music player now playing item changes (probably changes to another song),
-		// update the currentPlayingSong and upNextSongsQueue, the two observed variables in MusicPlayer.
-		.onReceive(NotificationCenter.default.publisher(
-			for: .MPMusicPlayerControllerNowPlayingItemDidChange)
-		) { _ in
-			musicPlayer.updateCurrentPlayingSong()
-		}
+                        
+                        /*
+                         musicManager.fetchStorefrontID(userToken: userToken) { storefrontID in
+                         print(storefrontID)
+                         musicManager.searchAppleMusic(userToken, storefrontID, "Taylor Swift") { songs in
+                         print(songs)
+                         }
+                         }
+                         */
+                    }
+                }
+            }
+        }
+        // When music player now playing item changes (probably changes to another song),
+        // update the currentPlayingSong and upNextSongsQueue, the two observed variables in MusicPlayer.
+        .onReceive(NotificationCenter.default.publisher(
+            for: .MPMusicPlayerControllerNowPlayingItemDidChange)
+        ) { _ in
+            musicPlayer.updateCurrentPlayingSong()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-		let musicManager = AppleMusicManager()
-		let fileHandler = FileHandler()
-		let musicFitPlaylistManager = MusicFitPlaylistManager(
-			musicManager: musicManager,
-			fileHandler: fileHandler
-		)
-		let contentViewIntentHandler = ContentViewIntentHandler(
-			musicFitPlaylistManager: musicFitPlaylistManager
-		)
-		let miniPlayerIntentHandler = MiniPlayerIntentHandler()
-		let musicPlayer = MusicPlayer(
-			fileHandler: fileHandler,
-			musicManager: musicManager,
-			previewSong: PreviewStatics.previewSong
-		)
-		let workoutManager = WorkoutManager(musicPlayer: musicPlayer)
-		
-		
-		ContentView(
-			musicManager: musicManager,
-			workoutManager: workoutManager,
-			musicPlayer: musicPlayer,
-			miniPlayerIntentHandler: miniPlayerIntentHandler,
-			fileHandler: fileHandler,
-			intentHandler: contentViewIntentHandler,
-			musicFitPlaylistManager: musicFitPlaylistManager
-		)
-			.preferredColorScheme(.dark)
-			.previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro Max"))
-		
-		ContentView(
-			musicManager: musicManager,
-			workoutManager: workoutManager,
-			musicPlayer: musicPlayer,
-			miniPlayerIntentHandler: miniPlayerIntentHandler,
-			fileHandler: fileHandler,
-			intentHandler: contentViewIntentHandler,
-			musicFitPlaylistManager: musicFitPlaylistManager
-		)
-			.preferredColorScheme(.light)
-			.previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro"))
+        let musicManager = AppleMusicManager()
+        let fileHandler = FileHandler()
+        let musicFitPlaylistManager = MusicFitPlaylistManager(
+            musicManager: musicManager,
+            fileHandler: fileHandler
+        )
+        let contentViewIntentHandler = ContentViewIntentHandler(
+            musicFitPlaylistManager: musicFitPlaylistManager
+        )
+        let miniPlayerIntentHandler = MiniPlayerIntentHandler()
+        let musicPlayer = MusicPlayer(
+            fileHandler: fileHandler,
+            musicManager: musicManager,
+            previewSong: PreviewStatics.previewSong
+        )
+        let workoutManager = WorkoutManager(musicPlayer: musicPlayer)
+        
+        
+        Group {
+            ContentView(
+                musicManager,
+                workoutManager,
+                musicPlayer,
+                miniPlayerIntentHandler,
+                fileHandler,
+                contentViewIntentHandler,
+                musicFitPlaylistManager
+            )
+            .preferredColorScheme(.dark)
+            .previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro Max"))
+            
+            ContentView(
+                musicManager,
+                workoutManager,
+                musicPlayer,
+                miniPlayerIntentHandler,
+                fileHandler,
+                contentViewIntentHandler,
+                musicFitPlaylistManager
+            )
+            .preferredColorScheme(.light)
+            .previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro"))
+        }
     }
 }
