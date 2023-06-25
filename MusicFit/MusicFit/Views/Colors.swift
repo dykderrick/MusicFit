1778fdf9e83a017946de8ab27da448253a215c51
Derrick Ding <yingke.dev@gmail.com>
2023-06-25 00:56:38 -0700

    add documentation to the extension of Color

--- MusicFit/MusicFit/Views/Colors.swift
+++ MusicFit/MusicFit/Views/Colors.swift
@@ -9,35 +9,44 @@ import Foundation
 import SwiftUI
 
 struct MusicFitColors {
-	static let green = "#25E495"
-	static let gray = "#D8D8D8"
+    static let green = "#25E495"
+    static let gray = "#D8D8D8"
 }
 
-// https://stackoverflow.com/questions/56874133/use-hex-color-in-swiftui
-// TODO: Add formal docs to this function.
 extension Color {
-	init(hex: String) {
-		let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
-		var int: UInt64 = 0
-		Scanner(string: hex).scanHexInt64(&int)
-		let a, r, g, b: UInt64
-		switch hex.count {
-		case 3: // RGB (12-bit)
-			(a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
-		case 6: // RGB (24-bit)
-			(a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
-		case 8: // ARGB (32-bit)
-			(a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
-		default:
-			(a, r, g, b) = (1, 1, 1, 0)
-		}
-
-		self.init(
-			.sRGB,
-			red: Double(r) / 255,
-			green: Double(g) / 255,
-			blue:  Double(b) / 255,
-			opacity: Double(a) / 255
-		)
-	}
+    /// A color specification by hex code.
+    /// Credit to the [Stack Overflow answer](https://stackoverflow.com/questions/56874133/use-hex-color-in-swiftui).
+    ///
+    /// ```
+    /// let customedWhite = Color(hex: "#FFFFFF")
+    /// let customedBlack = Color(hex: "000000")
+    /// ```
+    ///
+    /// - Parameters:
+    ///   - hex: A hex code `String` to represent a color. Can be a six-character-string with or without hashtag in the beginning.
+    ///
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
+        
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: Double(a) / 255
+        )
+    }
 }
